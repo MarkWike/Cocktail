e87d1e00b416f6340ab8aeeff342e865d6c97271
Mark <Mark.wike@gmail.com>
2022-04-07 21:17:59 +0100

    Initial Commit

---
+++ Cocktail/CocktailApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailApp.swift
+//  Cocktail
+//
+//  Created by Mark Wike on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CocktailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cocktail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cocktail
+//
+//  Created by Mark Wike on 07/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
