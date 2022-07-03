1d087c3585e9000e849e00dff166e5a3336b0d24
MarkWike <92692584+MarkWike@users.noreply.github.com>
2022-07-03 20:38:08 +0100

    Update UserDrinksView.swift

--- Cocktail/Views/UserDrinks/UserDrinksView.swift
+++ Cocktail/Views/UserDrinks/UserDrinksView.swift
@@ -26,161 +26,46 @@ struct UserDrinksView: View {
                                 LinearGradient(gradient: GradientColors, startPoint: .bottomLeading, endPoint: .center)
                                     .ignoresSafeArea()
                                 VStack{
-                                    Text(drinks.strDrink ?? "Drink")
-                                        .font(.custom("Futura Bold", size: 30))
-                                        .foregroundColor(.indigo)
-                                    Text(drinks.strAlcoholic ?? "Alcoholic")
-                                        .font(.custom("Futura", size: 20))
-                                        .frame(width: 300, alignment: .center)
-                                        .foregroundColor(.white)
-                                    let GlassText = "Best served in a \(drinks.strGlass?.lowercased() ?? "glass")"
-                                    
-                                
-                                    Text(GlassText)
-                                    
-                                        .font(.custom("Futura", size: 20))
-                                        .frame(width: 300, alignment: .center)
-                                        .foregroundColor(.white)
+                                    UserDrinkDrinkInfoView(drinkName: drinks.strDrink ?? "Drink", drinkType: drinks.strAlcoholic ?? "Alcoholic", drinkGlass: drinks.strGlass?.lowercased() ?? "glass")
                                     HStack {
                                         ScrollView(Axis.Set.vertical,showsIndicators: false) {
                                             VStack{
-                                                ZStack{
-                                                    RoundedRectangle(cornerRadius: 20)
-                                                        .fill(LinearGradient(gradient: GradientColors1, startPoint: .top, endPoint: .bottom))
-                                                    VStack {
-                                                        Text("Instructions")
-                                                            .font(.custom("Futura", size: 20))
-                                                            .frame(width: 300, alignment: .center)
-                                                            .foregroundColor(.white)
-                                                        Text(drinks.strInstructions ?? "instructions")
-                                                            .font(.custom("Futura", size: 15))
-                                                            .frame(width: 300, alignment: .center)
-                                                            .foregroundColor(.white)
-                                                            .padding(.horizontal)
-                                                            .padding(.bottom)
-                                                    }
-                                                }
+                                                userDrinksInstructionsView(drinkInstructions: drinks.strInstructions ?? "instructions")
                                                 .padding()
                                                 ZStack{
                                                     VStack(spacing: 20){
-                                                        Text("Ingredients")
-                                                            .font(.custom("Futura", size: 20))
-                                                            .shadow(color: .black.opacity(5.1), radius: 10, x: 5, y: 5)
-                                                            .frame(width: 300, alignment: .center)
-                                                            .foregroundColor(.white)
-                                                            .padding(20)
+                                                        userDrinksIngredientsHeaderView()
+
                                                         if drinks.strIngredient1 == "" {
                                                         }else{
-                                                            ZStack{
-                                                                RoundedRectangle(cornerRadius: 15.0)
-                                                                    .fill(LinearGradient(gradient: GradientColors1, startPoint: .top, endPoint: .bottom))
-                                                                    .frame(height: 40.0)
-                                                                    .padding(.horizontal)
-                                                                Text("\(drinks.strMeasure1 ?? "") \(drinks.strIngredient1 ?? "")")
-                                                                    .font(.custom("Futura", size: 15))
-                                                                    .foregroundColor(.white).opacity(1.6)
-                                                            }
+                                                            UserDrinksIngreientBoxView(drinkIngredient: drinks.strIngredient1 ?? "")
                                                         }
                                                         if drinks.strIngredient2 == "" {
                                                         }else{
-                                                            ZStack{
-                                                                RoundedRectangle(cornerRadius: 15.0)
-                                                                    .fill(LinearGradient(gradient: GradientColors1, startPoint: .top, endPoint: .bottom))
-                                                                    .frame(height: 40.0)
-                                                                    .padding(.horizontal)
-                                                                Text("\(drinks.strMeasure2 ?? "") \(drinks.strIngredient2 ?? "")")
-                                                                    .font(.custom("Futura", size: 15))
-                                                                    .foregroundColor(.white).opacity(1.6)
-                                                            }
+                                                            UserDrinksIngreientBoxView(drinkIngredient: drinks.strIngredient2 ?? "")
                                                         }
                                                         if drinks.strIngredient3 == ""  {
                                                         }else{
-                                                            ZStack{
-                                                                Spacer()
-                                                                RoundedRectangle(cornerRadius: 15.0)
-                                                                    .fill(LinearGradient(gradient: GradientColors1, startPoint: .top, endPoint: .bottom))
-                                                                    .frame(height: 40.0)
-                                                                    .padding(.horizontal)
-                                                                HStack{
-                                                                    Text("\(drinks.strMeasure3 ?? "") \(drinks.strIngredient3 ?? "")")
-                                                                        .font(.custom("Futura", size: 15))
-                                                                        .foregroundColor(.white).opacity(1.6)
-                                                                }
-                                                                Spacer()
-                                                            }
+                                                            UserDrinksIngreientBoxView(drinkIngredient: drinks.strIngredient3 ?? "")
                                                             if drinks.strIngredient4 == "" {
                                                             }else{
-                                                               
-                                                                ZStack{
-                                                                   
-                                                                    
-                                                                    Spacer()
-                                                                    RoundedRectangle(cornerRadius: 15.0)
-                                                                        .fill(LinearGradient(gradient: GradientColors1, startPoint: .top, endPoint: .bottom))
-                                                                        .frame(height: 40.0)
-                                                                        .padding(.horizontal)
-                                                                    Text("\(drinks.strMeasure4 ?? "") \(drinks.strIngredient4 ?? "")")
-                                                                        .font(.custom("Futura", size: 15))
-                                                                        .foregroundColor(.white).opacity(1.6)
-                                                                    Spacer()
-                                                                }
+                                                                UserDrinksIngreientBoxView(drinkIngredient: drinks.strIngredient4 ?? "")
                                                             }
                                                             if drinks.strIngredient5 == "" {
                                                             }else{
-                                                                ZStack{
-                                                                    Spacer()
-                                                                    RoundedRectangle(cornerRadius: 15.0)
-                                                                        .fill(LinearGradient(gradient: GradientColors1, startPoint: .top, endPoint: .bottom))
-                                                                        .frame(height: 40.0)
-                                                                        .padding(.horizontal)
-                                                                    Text("\(drinks.strMeasure5 ?? "") \(drinks.strIngredient5 ?? "")")
-                                                                        .font(.custom("Futura", size: 15))
-                                                                        .foregroundColor(.white).opacity(1.6)
-                                                                    Spacer()
-                                                                }
+                                                                UserDrinksIngreientBoxView(drinkIngredient: drinks.strIngredient5 ?? "")
                                                             }
                                                             if drinks.strIngredient6 == "" {
                                                             }else{
-                                                                ZStack{
-                                                                    Spacer()
-                                                                    RoundedRectangle(cornerRadius: 15.0)
-                                                                        .fill(LinearGradient(gradient: GradientColors1, startPoint: .top, endPoint: .bottom))
-                                                                        .frame(height: 40.0)
-                                                                        .padding(.horizontal)
-                                                                    Text("\(drinks.strMeasure6 ?? "") \(drinks.strIngredient6 ?? "")")
-                                                                        .font(.custom("Futura", size: 15))
-                                                                        .foregroundColor(.white).opacity(1.6)
-                                                                    Spacer()
-                                                                }
+                                                                UserDrinksIngreientBoxView(drinkIngredient: drinks.strIngredient6 ?? "")
                                                             }
                                                             if drinks.strIngredient7 == "" {
                                                             }else{
-                                                                ZStack{
-                                                                    Spacer()
-                                                                    RoundedRectangle(cornerRadius: 15.0)
-                                                                        .fill(LinearGradient(gradient: GradientColors1, startPoint: .top, endPoint: .bottom))
-                                                                        .frame(height: 40.0)
-                                                                        .padding(.horizontal)
-                                                                    Text("\(drinks.strMeasure7 ?? "") \(drinks.strIngredient7 ?? "")")
-                                                                        .font(.custom("Futura", size: 15))
-                                                                        .foregroundColor(.white).opacity(1.6)
-                                                                    Spacer()
-                                                                }
+                                                                UserDrinksIngreientBoxView(drinkIngredient: drinks.strIngredient7 ?? "")
                                                             }
                                                             if drinks.strIngredient8 == "" {
-                                                                
                                                             }else{
-                                                                ZStack{
-                                                                    Spacer()
-                                                                    RoundedRectangle(cornerRadius: 1.0)
-                                                                        .fill(LinearGradient(gradient: GradientColors1, startPoint: .top, endPoint: .bottom))
-                                                                        .frame(height: 80.0)
-                                                                       // .padding(.horizontal)
-                                                                    Text("\(drinks.strMeasure8 ?? "") \(drinks.strIngredient8 ?? "")")
-                                                                        .font(.custom("Futura", size: 15))
-                                                                        .foregroundColor(.white).opacity(1.6)
-                                                                    Spacer()
-                                                                }
+                                                                UserDrinksIngreientBoxView(drinkIngredient: drinks.strIngredient8 ?? "")
                                                             }
                                                         }
                                                     }
@@ -192,16 +77,7 @@ struct UserDrinksView: View {
                                 }
                             }
                         } label: {
-                            VStack{
-                                Text(drinks.strDrink ?? "")
-                                    .font(.custom("Futura", size: 25))
-                                    .foregroundColor(.white)
-                                    .frame(width: 200, height: 50, alignment: .leading)
-                                    .shadow(color: .indigo.opacity(0.9), radius: 10, x: 5, y: 5)
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(LinearGradient(gradient: GradientColors2, startPoint: .leading, endPoint: .trailing))
-                                    .frame(width: 250, height: 3, alignment: .trailing)
-                            }
+                            UserDrinkMenuListView(drinkName: drinks.strDrink ?? "")
                         }
                     }.listRowBackground(Color.clear)
                 }
